54c2e42b9b55d33ff7b68bcae910d8c64cd27dad
akhorn <anais.khorn@etu.unice.fr>
2020-03-12 15:06:04 +0100

    Create cas & temoignages components

---
+++ src/app/temoignages/temoignages.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TemoignagesComponent } from './temoignages.component';
+
+describe('TemoignagesComponent', () => {
+  let component: TemoignagesComponent;
+  let fixture: ComponentFixture<TemoignagesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TemoignagesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TemoignagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
