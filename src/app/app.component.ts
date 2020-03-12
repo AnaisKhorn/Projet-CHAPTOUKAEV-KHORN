f6b541ed96eb1317ef7958d2b70ef717539c9417
akhorn <anais.khorn@etu.unice.fr>
2020-03-12 15:09:01 +0100

    Some modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Cas et témoignages d\'objets célestes non identifiés';
+  subtitle = 'Par Hava CHAPTOUKAEV & Anais KHORN';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CasComponent } from './cas/cas.component';
+import { TemoignagesComponent } from './temoignages/temoignages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasComponent,
+    TemoignagesComponent
   ],
   imports: [
     BrowserModule,
